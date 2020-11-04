cbcaa1090921ab13282a9057b0dad2136165be00
Will Studabaker <will.studabaker@gmail.com>
2020-11-04 16:16:45 +0000

    Automatically backed up by Learn

--- lib/triangle.rb
+++ lib/triangle.rb
@@ -1,11 +1,11 @@
 class Triangle
-  attr_accessor :side_1, :side_2, :side_3
+#  attr_accessor :side_1, :side_2, :side_3
   def initialize(side_1, side_2, side_3)
     @@sides = [side_1, side_2, side_3]
   end
   def kind
-    @@sides.sort
-    if self.side_1 == self.side_2 && self.side_1 == self.side_3
+    s_sides = @@sides.sort
+    if s_sides[0] == s_sides [1] && s_sides[0] == s_sides[2]
       :equilateral
     elsif
 

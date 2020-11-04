8ad322d8ec2c9f969df36137e0370325c57276d1
Will Studabaker <will.studabaker@gmail.com>
2020-11-04 16:26:54 +0000

    Automatically backed up by Learn

--- lib/triangle.rb
+++ lib/triangle.rb
@@ -5,7 +5,7 @@ class Triangle
   end
   def kind
     s_sides = @@sides.sort
-    if #not a valid triangle raise custom error
+    if !valid_trianlge?
       raise TriangleError
     else
       if s_sides[0] == s_sides[1] && s_sides[0] == s_sides[2]

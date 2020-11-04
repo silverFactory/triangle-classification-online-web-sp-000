eff4bc79aba436f2b14c205c9273fda8a2c302fb
Will Studabaker <will.studabaker@gmail.com>
2020-11-04 16:15:03 +0000

    Automatically backed up by Learn

--- lib/triangle.rb
+++ lib/triangle.rb
@@ -4,6 +4,7 @@ class Triangle
     @@sides = [side_1, side_2, side_3]
   end
   def kind
+    @@sides.sort
     if self.side_1 == self.side_2 && self.side_1 == self.side_3
       :equilateral
     elsif
